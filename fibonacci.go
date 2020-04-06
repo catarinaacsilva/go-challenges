2e7c1c4be7f3ad3f9ae8857a85e93f800fdb08c8
Catarina Silva <c.alexandracorreia@ua.pt>
2020-04-07 00:08:32 +0100

    Add fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func fibonacci(n int) int{ 
+	if n == 1{
+		return 0
+	} else if n == 2{
+		return 1
+	}
+	return fibonacci(n-1)+fibonacci(n-2)
+}
+
+func main(){
+	fmt.Println(fibonacci(8))
+}
