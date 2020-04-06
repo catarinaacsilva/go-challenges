22a7740fd1aa47a158fc2e28b1aeebbbf4933ff2
Catarina Silva <c.alexandracorreia@ua.pt>
2020-04-06 20:00:42 +0100

    Update initial file

--- README.md
+++ README.md
@@ -22,6 +22,17 @@
 
 2. To assign result of function variable: `x := function()`
 
+3. The `var` statement declares a list of variables
+
+4. Inside a function, the `:=` short assignment statement can be used in place of a `var` declaration with implicit type. 
+
+5. When declaring a variable without specifying an explicit type (either by using the := syntax or var = expression syntax), the variable's type is inferred from the value on the right hand side. This is on 9 case at main of hello.go.
+
+6. Constants are declared like variables, but with the `const` keyword.
+
+7. Constants can be character, string, boolean, or numeric values.
+
+8. Constants cannot be declared using the `:=` syntax. 
 
 ## Authors
 
--- hello.go
+++ hello.go
@@ -1,13 +1,90 @@
 package main
 
-import "fmt"
+// import "fmt"
+
+import(
+	"fmt"
+	"math/cmplx"
+	"math"
+)
+
+var c, python, java bool
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
 
 func add(x int, y int) int {
 	return x + y
 }
 
+/*
+ * Split value
+ * Input: int that represents the sum
+ * Output: x and y (both int)
+ * Note: (x int, y int) = (x,y int)
+ */
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	//fmt.Println(x)
+	//fmt.Println(y)
+	return
+}
+
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
 func main() {
 	fmt.Printf("hello, world\n")
+
 	result := add(2, 3)
 	fmt.Println(result)
+
+	fmt.Println(split(17))
+
+	var i int
+	python = true // just test
+	fmt.Println(i, c, python, java)
+
+	var j, k int = 1, 2
+	fmt.Printf("j=%d \nk=%d\n", j, k)
+
+	var l, m int = 1, 2
+	n := 3
+	c, python, java := true, false, "no!"
+	fmt.Println(l, m, n, c, python, java)
+
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+
+	v := 42
+	p := 42.2
+	fmt.Printf("v is of type %T and n is of type %T\n", v, p)
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
