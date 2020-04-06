bddfbb0a3c5a07120504e91d16894c05a9a556ac
Catarina Silva <c.alexandracorreia@ua.pt>
2020-04-07 00:27:45 +0100

    Finish fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-
-func fibonacci(n int) int{ 
-	if n == 1{
-		return 0
-	} else if n == 2{
-		return 1
-	}
-	return fibonacci(n-1)+fibonacci(n-2)
-}
-
 func main(){
-	fmt.Println(fibonacci(8))
+	var p, c, r int = 1,2,2
+
+	for c < 4000000{
+		t := c
+		c = c + p
+		p = t
+		if c%2 == 0{
+			r += c
+		}
+	}
+	fmt.Printf("Result: %d\n", r)
 }
